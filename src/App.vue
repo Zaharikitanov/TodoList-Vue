8b96d71718d31e97750bf7bfae4120de4e69488b
Zahari Kitanov <zahari.kitanov@abv.bg>
2019-05-27 11:14:19 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="app">
+        <Header />
+        <router-view />
+    </div>
+</template>
+
+<script>
+    import Header from './components/layout/Header';
+
+    export default {
+        name: "app",
+        components: {
+            Header
+        }
+    }
+</script>
+
+<style>
+
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
+    .btn {
+        display: inline-block;
+        border: none;
+        background: #555;
+        color: #fff;
+        padding: 7px 20px;
+        cursor: pointer;
+    }
+
+        .btn:hover {
+            background: #666;
+        }
+</style>
